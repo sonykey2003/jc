9f38e48ce8ce4ad866218b8a7d69be9261303cbc
sonykey2003 <sonykey2003@gmail.com>
2023-09-18 11:22:29 +0800

    updating

--- get-jcOSQueryResult.ps1
+++ get-jcOSQueryResult.ps1
@@ -1,39 +1,64 @@
 
+# -----------------------------------------------------------------------------
+# Script: get-jcOSQueryResult.ps1
+# Version: 1.0.3
+# Author: Shawn Song
+# Reference: 
+#  -  https://github.com/TheJumpCloud/jcapi-powershell/blob/master/SDKs/PowerShell/JumpCloud.SDK.V1/examples/Search-JcSdkCommandResult.md
+# 
+# Notes: Always name your cmd with a proper naming covention
+# -----------------------------------------------------------------------------
 
-$cmdName = "Edge Extensions - JCQSQuery"
-#@{'and' = @("name:`$regex:RadiusCert*", "workflowId:`$eq:6406532738e1a32cafa24260")}
-$commandsResultBody = @{
-    filter = @{
-        'and' = @("name:`$eq:$cmdName" )
+# Input the cmd name you use for querying the Edge/Firefox add-ons
+$cmdName = "Edge Extensions - JCQSQuery" # Microsft Edge Browser 
+#$cmdName = "FF Add-ons - JCQSQuery" # Firefox Browser
+
+
+#################################### Don't Change the code below this line ####################################
+function get-jcOSQueryResults {
+    param (
+        [string]$cmdName
+    )
+    $commandsResultBody = @{
+        filter = @{
+            'and' = @("name:`$eq:$cmdName" )
+        }
+        fields = 'response.data.exitCode response.data.output system responseTime'
+    }| ConvertTo-Json -Depth 99
+    $results = Search-JcSdkCommandResult -body $commandsResultBody 
+    return $results
+}
+
+function outputOSQReport {
+    # Build the array for outputting to csv
+    $outPutResults = @()
+
+    # Getting the cmd results with the json output
+    $results = get-jcOSQueryResults($cmdName)
+
+    # Getting the extension info from each cmd result
+    foreach ($r in $results){
+        $ext = $r.DataOutput | ConvertFrom-Json -Depth 99 
+        foreach ($e in $ext){
+            $tempOutput = "" | select dataCollectTime,name,author,identifier,install_time,manifest_hash,path,permissions,profile,profile_path,version,systemID
+            
+            $tempOutput.dataCollectTime = $r.responseTime
+            $tempOutput.name = $e.name
+            $tempOutput.author = $e.author
+            $tempOutput.identifier = $e.identifier
+            $tempOutput.install_time  = $e.install_time
+            $tempOutput.manifest_hash = $e.manifest_hash
+            $tempOutput.path = $e.path
+            $tempOutput.permissions = $e.permissions
+            $tempOutput.profile = $e.profile
+            $tempOutput.profile_path = $e.profile_path
+            $tempOutput.version = $e.version
+            $tempOutput.systemID = $r.systemId
+            $outPutResults += $tempOutput
+        }
     }
-    fields = 'response.data.exitCode response.data.output system'
-}| ConvertTo-Json -Depth 99
-$results = Search-JcSdkCommandResult -body $commandsResultBody 
-
-$outPutResults = @()
-
-foreach ($r in $results){
-    $edgeExt = $r.DataOutput | ConvertFrom-Json -Depth 99 
-    foreach ($e in $edgeExt){
-        $tempOutput = "" | select author,browser_type,from_webstore,identifier,install_time,manifest_hash,path,permissions,profile,profile_path,version,systemID
-        $tempOutput.author = $e.author
-        $tempOutput.browser_type = $e.browser_type
-        $tempOutput.from_webstore = $e.from_webstore
-        $tempOutput.identifier = $e.identifier
-        $tempOutput.install_time  = $e.install_time
-        $tempOutput.manifest_hash = $e.manifest_hash
-        $tempOutput.path = $e.path
-        $tempOutput.permissions = $e.permissions
-        $tempOutput.profile = $e.profile
-        $tempOutput.profile_path = $e.profile_path
-        $tempOutput.version = $e.version
-        $tempOutput.systemID = $r.systemId
-        $outPutResults += $tempOutput
-    }
+    $outPutResults | Export-Csv ".\$((Get-Date).ToString("yyyyMMdd"))_ExtReport.csv"
     
 }
 
-
-
-
-$outPutResults | Export-Csv edgeExtReport.csv
+outputOSQReport
